c6d3d7a55db75fffe050a16ab51e0de6deabb7e8
Dave <Dave@localhost>
2010-07-27 07:58:07 +0200

    - outlined demonstration groups - added some skeleton demonstrations

---
+++ Adam.JSGenerator.Demonstration/Demonstrations/ObjectLiteralsDemonstration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adam.JSGenerator.Demonstration.Demonstrations
+{
+    [Description("Simple Object Literals")]
+    [Group(Group.ObjectLiterals, 0)]
+    class ObjectLiteralsDemonstration : Demonstration
+    {
+        public override object Run()
+        {
+            return string.Empty;
+        }
+    }
+}
