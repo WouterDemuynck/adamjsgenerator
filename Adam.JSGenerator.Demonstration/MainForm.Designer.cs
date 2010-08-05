e7a48dae77d28be20b7fa16ba40714be688aa084
Dave <dave@adamsoftware.net>
2010-08-05 18:36:31 +0200

    - Added some more examples - Added the JQuery namespace

--- Adam.JSGenerator.Demonstration/MainForm.Designer.cs
+++ Adam.JSGenerator.Demonstration/MainForm.Designer.cs
@@ -32,6 +32,7 @@
             System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Object Literals", System.Windows.Forms.HorizontalAlignment.Left);
             System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("Statements", System.Windows.Forms.HorizontalAlignment.Left);
             System.Windows.Forms.ListViewGroup listViewGroup4 = new System.Windows.Forms.ListViewGroup("... and LINQ", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup5 = new System.Windows.Forms.ListViewGroup("Other", System.Windows.Forms.HorizontalAlignment.Left);
             this.listPanel = new System.Windows.Forms.Panel();
             this.demonstrationList = new System.Windows.Forms.ListView();
             this.hostPanel = new System.Windows.Forms.Panel();
@@ -67,11 +68,14 @@
             listViewGroup3.Name = "statementsGroup";
             listViewGroup4.Header = "... and LINQ";
             listViewGroup4.Name = "linqGroup";
+            listViewGroup5.Header = "Other";
+            listViewGroup5.Name = "otherGroup";
             this.demonstrationList.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
             listViewGroup1,
             listViewGroup2,
             listViewGroup3,
-            listViewGroup4});
+            listViewGroup4,
+            listViewGroup5});
             this.demonstrationList.HideSelection = false;
             this.demonstrationList.Location = new System.Drawing.Point(0, 0);
             this.demonstrationList.MultiSelect = false;
