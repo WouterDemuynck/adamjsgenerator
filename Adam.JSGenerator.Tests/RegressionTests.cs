3a57ec1ebaa98e760e42da79588915bdc253014b
Dave Van den Eynde <DaveVdE@users.noreply.github.com>
2011-03-23 15:01:29 +0100

    This got left out of previous commit?

---
+++ Adam.JSGenerator.Tests/RegressionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable InconsistentNaming
+namespace Adam.JSGenerator.Tests
+{
+    [TestClass]
+    public class RegressionTests
+    {
+        [TestMethod]
+        public void Item_10_StrangeOutputWithDoublesInArraysInSomeLocales()
+        {
+            var expression = JS.Var(JS.Id("blah")).AssignWith(new double[] { 390.53466354643, 2.6 });
+
+            Assert.AreEqual("var blah=[390.53466354643,2.6];", expression.ToString());
+        }
+    }
+}
+// ReSharper restore InconsistentNaming
