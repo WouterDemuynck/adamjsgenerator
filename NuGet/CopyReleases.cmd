fbade6ec4b3ceffbf116e9e5ff3b2a9a2659cbd5
Dave <dave@adamsoftware.net>
2011-12-28 14:02:03 +0100

    - Final fixes. - Changed version number. - Upgraded to latest version of NuGet tools. - Built the NuGet package using this version.

--- NuGet/CopyReleases.cmd
+++ NuGet/CopyReleases.cmd
@@ -1,2 +1,3 @@
 copy /y ..\Adam.JSGenerator\bin\Release\Adam.JSGenerator.dll Adam.JSGenerator\lib\net35
+copy /y ..\Adam.JSGenerator\bin\Release\Adam.JSGenerator.pdb Adam.JSGenerator\lib\net35
 copy /y ..\Adam.JSGenerator\bin\Release\Adam.JSGenerator.xml Adam.JSGenerator\lib\net35
