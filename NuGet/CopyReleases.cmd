920f0a22986d2263ec5ef695944faa04dea92760
Wouter Demuynck <wouter@adamsoftware.net>
2016-03-23 16:29:10 +0100

    Updated release workflow.

--- NuGet/CopyReleases.cmd
+++ NuGet/CopyReleases.cmd
@@ -1,3 +1,5 @@
+@ECHO OFF
+mkdir Adam.JSGenerator\lib\net35
 copy /y ..\Adam.JSGenerator\bin\Release\Adam.JSGenerator.dll Adam.JSGenerator\lib\net35
 copy /y ..\Adam.JSGenerator\bin\Release\Adam.JSGenerator.pdb Adam.JSGenerator\lib\net35
 copy /y ..\Adam.JSGenerator\bin\Release\Adam.JSGenerator.xml Adam.JSGenerator\lib\net35
